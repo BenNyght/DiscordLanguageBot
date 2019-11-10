3b40838425203cd1a8392f814afcb5e7b8d3a90a
Benjamin <mail.benjaminfinlay@gmail.com>
2019-11-11 02:32:52 +1000

    bot

--- bot.py
+++ bot.py
@@ -1,16 +1,54 @@
-# bot.py
-import os
+import sys
+from typing import Dict
 
+import random
 import discord
-from dotenv import load_dotenv
+from discord.errors import Forbidden
+from discord.guild import Guild
+from discord.message import Message
+from discord.reaction import Reaction
+from discord.user import User
+from discord.ext import commands
+from discord.colour import Colour
 
-load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+from warning import get_warning
+from warning import WARNINGS
 
-client = discord.Client()
+bot = discord.Client()
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        await message.add_reaction("❎")
+        return
 
-client.run(token)
+    # Bad word check
+    data = get_warning(message.content)
+    if data is not None:
+        await message.add_reaction("🙊")
+
+    # Bot Test
+    if message.content == "!!!testLanguageBot":
+        await message.channel.send(message.content)
+
+@bot.event
+async def on_reaction_add(reaction, user):
+        reactionCount = reaction.count
+        if reaction.me:
+            reactionCount = reactionCount - 1
+        if reaction.message.author == bot.user:
+            if reactionCount > 0 and reaction.emoji == "❎":
+                await reaction.message.delete()
+        elif reactionCount > 0 and reaction.emoji == "🙊":
+            await reaction.message.remove_reaction("🙊", bot.user)
+            await reaction.message.remove_reaction("🙊", user)
+            embed = get_embed_message(get_warning(reaction.message.content))
+            await reaction.message.channel.send(embed=embed)
+
+def get_embed_message(warning):
+    colour = discord.Colour(random.randint(0, 0xFFFFFF))
+    new_colour = discord.Colour.from_rgb(*(round(c * 0.7) for c in colour.to_rgb()))
+    embed = discord.Embed(title="Language Suggester", description=warning + "\n\nThis bot exists simply to ask you to reflect on your word choices, and will never moderate or punish people for activating it" + "\n\nIf you have an issue with the bot, a change for a words description or additional words for the bot please message @Nyght", color=new_colour)
+    return embed
+
+bot.run("")
